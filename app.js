e75853b996f3df089504417ef1160b603d104605
XamNiner <xamniner@fivedigits.net>
2016-07-27 12:50:25 +0200

    added angular main

---
+++ app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name chatApp
+ * @description
+ * # chatApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('chatApp', [
+    ]);
+  
---
+++ chat.js
@@ -0,0 +1,9 @@
+//Immediately Invoked Function Expression (IIFE)
+(function() {
+'use strict';
+
+angular.module('chatApp')
+.controller('ChatCtrl', function($scope, socket) {
+    
+})
+})();
