e968c18cd8783cbb4ecb1beb24c8adffc6d3326b
XamNiner <xamniner@fivedigits.net>
2016-07-27 12:44:21 +0200

    added index from pip

--- server.js
+++ server.js
@@ -13,6 +13,11 @@ const server = express()
 
 const io = socketIO(server);
 
+io.configure(function () {
+  io.set("transports", ["xhr-polling"]);
+  io.set("polling duration", 10);
+});
+
 io.on('connection', (socket) => {
   console.log('Client connected');
   socket.on('disconnect', () => console.log('Client disconnected'));
