342bdbc6c11b79a8f499782a3afa52e83526a891
XamNiner <xamniner@fivedigits.net>
2016-08-19 09:57:52 +0200

    navbar image changes

--- pip/photo/thumbDirective.js
+++ pip/photo/thumbDirective.js
@@ -2,21 +2,25 @@
 'use strict';
     
 angular.module('chatApp').directive('thumbDisplay', function() {
-    var controller = function() {
-        var vm = this;
-        console.log('<<<<<<<<<<<DIRECTIVE THUMBNAILS');
-        console.log('Container content: '+vm.imgContainer);
-    }
-    
     return {
-        restrict: 'EA', //can be used as element or attribute
+        restrict: 'EA',
+        replace: true,
         scope: {
-            imgContainer: '='
+            imgSrc: '@',
+            time: '@'
         },
-        controller: controller,
-        controllerAs: 'vm',
-        bindToController: true,
-        templateUrl: '/views/thumbnails.html'    
+        template: [
+            '<div class="dropdown thumbs"> ',
+            '   <img ng-src="{{imgSrc}}" class="img-responsive thumbnail" width="100" height="100">',
+            '   <div class="dropdown-content thumbs-content">',
+            '       <img ng-src="{{imgSrc}}" class="img-responsive thumbnail" width="640" height="480">',
+            '       <div class="desc">{{time}}</div>',
+            '   </div>',
+            '</div>'
+        ].join(''),
+        controller: ['$scope', function($scope) {
+            console.log('---Creating new thumbnail---');
+        }]
     };
 })       
 })()
